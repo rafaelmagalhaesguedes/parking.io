a4679a1818cfd48537129201c859c821a28c6e8d
Rafael <rafaelmagalhaes1@live.com>
2023-07-24 09:47:31 -0300

    feat: add função exibirVeiculos()

--- js/app.js
+++ js/app.js
@@ -1,117 +1,155 @@
-class Veiculo {
-  constructor(placa, entrada) {
-    this.placa = placa;
-    this.entrada = entrada;
-  }
-}
+/*
+  Sistema Estacionamento JS
 
-class Estacionamento {
-  constructor() {
-    this.veiculos = this.obterVeiculos();
-  }
+  Autor: Rafael Guedes -> cyberrminfo@gmail.com
 
-  adicionarVeiculo(placa) {
-    if (placa !== "") {
-      const veiculo = new Veiculo(placa, new Date());
-      this.veiculos.push(veiculo);
-      this.salvarVeiculos();
-    }
-  }
+  -----> JavaScript + LocalStorage
 
-  obterVeiculos() {
-    const veiculos = localStorage.getItem("veiculos");
-    return veiculos ? JSON.parse(veiculos) : [];
-  }
+*/
 
-  removerVeiculo(placa) {
-    const indice = this.veiculos.findIndex((veiculo) => veiculo.placa === placa);
-    if (indice !== -1) {
-      const veiculo = this.veiculos[indice];
-      this.veiculos.splice(indice, 1);
-      this.salvarVeiculos();
-
-      const permanencia = this.calcularPermanencia(veiculo.entrada, new Date());
-      const valorAPagar = this.calcularValorAPagar(permanencia);
-      
-      alert(`Tempo de permanência: ${permanencia.horas} horas, ${permanencia.minutos} minutos, ${permanencia.segundos} segundos\nValor a pagar: R$${valorAPagar.toFixed(2)}`);
-    }
-  }
-
-  calcularPermanencia(entrada, saida) {
-    const diff = Math.abs(saida - entrada) / 1000; // diferença em segundos
-
-    const horas = Math.floor(diff / 3600);
-    const minutos = Math.floor((diff % 3600) / 60);
-    const segundos = Math.floor(diff % 60);
-
-    return { horas, minutos, segundos };
-  }
-
-  calcularValorAPagar(permanencia) {
-    // Lógica para cálculo do valor a pagar, você pode personalizá-la conforme suas regras de cobrança
-    const valorPorHora = 10;
-    const totalHoras = permanencia.horas + permanencia.minutos / 60 + permanencia.segundos / 3600;
-    const valorAPagar = valorPorHora * totalHoras;
-    return valorAPagar;
-  }
-
-  salvarVeiculos() {
-    localStorage.setItem("veiculos", JSON.stringify(this.veiculos));
-  }
-
-  exibirVeiculos() {
-    const listaVeiculos = document.getElementById("listaVeiculos");
-    listaVeiculos.innerHTML = "";
-
-    for (const veiculo of this.veiculos) {
-      const itemLista = document.createElement("li");
-      itemLista.classList.add("list-group-item");
-      itemLista.textContent = "Placa: " + veiculo.placa;
-
-      const botaoRemover = document.createElement("button");
-      botaoRemover.classList.add("btn", "btn-danger", "btn-sm", "ml-2");
-      botaoRemover.textContent = "Remover";
-      botaoRemover.addEventListener("click", () => {
-        this.removerVeiculo(veiculo.placa);
-        this.exibirVeiculos();
-      });
-
-      itemLista.appendChild(botaoRemover);
-      listaVeiculos.appendChild(itemLista);
-    }
-  }
-}
-
-const estacionamento = new Estacionamento();
-
-// Função para carregar os dados na página inicial
-function carregarDadosIniciais() {
-  estacionamento.exibirVeiculos();
-}
 // Função para adicionar um veículo
-function adicionarVeiculo() {
-  const placa = document.getElementById("placa").value;
-  estacionamento.adicionarVeiculo(placa);
-  estacionamento.exibirVeiculos();
+const adicionarVeiculo = (placa) => {
+  if (placa !== "") {
+    // Cria um objeto "veiculo" com os dados da placa e a hora de entrada
+    const veiculo = {
+      placa: placa,
+      entrada: new Date()
+    };
+    const veiculosEstacionados = obterVeiculos();
+    veiculosEstacionados.push(veiculo);
+    salvarVeiculos(veiculosEstacionados);
+    exibirVeiculos();
+    limparCampos();
+  }
+};
+
+// Função para obter a lista de veículos estacionados do localStorage
+const obterVeiculos = () => {
+  const veiculos = localStorage.getItem("veiculos");
+  return veiculos ? JSON.parse(veiculos) : [];
+};
+
+// Função para remover um veículo pelo número da placa
+const removerVeiculo = (placa) => {
+  const veiculosEstacionados = obterVeiculos();
+  const indice = veiculosEstacionados.findIndex((veiculo) => veiculo.placa === placa);
+  if (indice !== -1) {
+    const veiculo = veiculosEstacionados[indice];
+    veiculosEstacionados.splice(indice, 1);
+    salvarVeiculos(veiculosEstacionados);
+
+    // Calcula a permanência do veículo e o valor a pagar
+    const permanencia = calcularPermanencia(new Date(veiculo.entrada), new Date());
+    const valorAPagar = calcularValorAPagar(permanencia);
+    alert(`Tempo de permanência: ${permanencia.horas} horas, ${permanencia.minutos} minutos, ${permanencia.segundos} segundos\nValor a pagar: R$${valorAPagar.toFixed(2)}`);
+    exibirVeiculos();
+  }
+};
+
+// Função para calcular a permanência de um veículo
+const calcularPermanencia = (entrada, saida) => {
+  const diff = Math.abs(saida - entrada) / 1000; // diferença em segundos
+  const horas = Math.floor(diff / 3600);
+  const minutos = Math.floor((diff % 3600) / 60);
+  const segundos = Math.floor(diff % 60);
+  return { horas, minutos, segundos };
+};
+
+// Função para calcular o valor a pagar pela permanência do veículo
+const calcularValorAPagar = (permanencia) => {
+  // Lógica para cálculo do valor a pagar, você pode personalizá-la conforme suas regras de cobrança
+  const valorPorHora = 10;
+  const totalHoras = permanencia.horas + permanencia.minutos / 60 + permanencia.segundos / 3600;
+  const valorAPagar = valorPorHora * totalHoras;
+  return valorAPagar;
+};
+
+// Função para salvar a lista de veículos no localStorage
+const salvarVeiculos = (veiculos) => {
+  localStorage.setItem("veiculos", JSON.stringify(veiculos));
+};
+
+// Função para exibir a lista de veículos na página
+function exibirVeiculos() {
+  const listaVeiculos = document.getElementById("listaVeiculos");
+  listaVeiculos.innerHTML = "";
+
+  const veiculosEstacionados = obterVeiculos();
+  veiculosEstacionados.forEach((veiculo) => {
+    const tr = document.createElement("tr");
+
+    const tdPlaca = document.createElement("td");
+    tdPlaca.textContent = veiculo.placa;
+    tr.appendChild(tdPlaca);
+
+    const tdDataEntrada = document.createElement("td");
+    const dataEntradaFormatada = formatarData(veiculo.entrada);
+    tdDataEntrada.textContent = dataEntradaFormatada.data;
+    tr.appendChild(tdDataEntrada);
+
+    const tdHoraEntrada = document.createElement("td");
+    const horaEntradaFormatada = formatarHora(veiculo.entrada);
+    tdHoraEntrada.textContent = horaEntradaFormatada;
+    tr.appendChild(tdHoraEntrada);
+
+    const tdBotaoRemover = document.createElement("td");
+    const botaoRemover = document.createElement("button");
+    botaoRemover.classList.add("btn", "btn-danger", "btn-sm");
+    botaoRemover.textContent = "Remover";
+    botaoRemover.addEventListener("click", () => {
+      removerVeiculo(veiculo.placa);
+    });
+    tdBotaoRemover.appendChild(botaoRemover);
+    tr.appendChild(tdBotaoRemover);
+
+    listaVeiculos.appendChild(tr);
+  });
 }
 
-// Função para buscar veículos por placa
-function buscarPlaca() {
+// Função para formatar a data da entrada do veículo
+function formatarData(dataHora) {
+  const data = new Date(dataHora);
+  const dia = data.getDate().toString().padStart(2, "0");
+  const mes = (data.getMonth() + 1).toString().padStart(2, "0");
+  const ano = data.getFullYear();
+  return { data: `${dia}/${mes}/${ano}` };
+}
+
+// Função para formatar a hora da entrada do veículo
+function formatarHora(dataHora) {
+  const data = new Date(dataHora);
+  const hora = data.getHours().toString().padStart(2, "0");
+  const minutos = data.getMinutes().toString().padStart(2, "0");
+  return `${hora}:${minutos}`;
+}
+
+// Função para limpar o campo input
+const limparCampos = () => {
+  document.getElementById("placa").value = "";
+};
+
+//
+// Event listeners para os botões e carregamento inicial da página
+//
+
+// Adicionar
+document.getElementById("adicionarBtn").addEventListener("click", () => {
+  const placa = document.getElementById("placa").value;
+  adicionarVeiculo(placa);
+});
+
+// Buscar
+document.getElementById("buscarBtn").addEventListener("click", () => {
   const buscaPlacaInput = document.getElementById("placa");
   const termoBusca = buscaPlacaInput.value.trim().toLowerCase();
 
   if (termoBusca === "") {
-    // Se o campo de busca estiver vazio, exibir todos os veículos
-    estacionamento.exibirVeiculos();
+    exibirVeiculos();
   } else {
-    // Filtrar os veículos pelo número da placa
-    const veiculosFiltrados = estacionamento.veiculos.filter(veiculo => veiculo.placa.toLowerCase().includes(termoBusca));
-
-    // Atualizar a lista com os veículos filtrados
+    const veiculosFiltrados = obterVeiculos().filter((veiculo) => veiculo.placa.toLowerCase().includes(termoBusca));
     const listaVeiculos = document.getElementById("listaVeiculos");
     listaVeiculos.innerHTML = "";
-
-    for (const veiculo of veiculosFiltrados) {
+    veiculosFiltrados.forEach((veiculo) => {
       const itemLista = document.createElement("li");
       itemLista.classList.add("list-group-item");
       itemLista.textContent = veiculo.placa;
@@ -120,21 +158,19 @@ function buscarPlaca() {
       botaoRemover.classList.add("btn", "btn-danger", "btn-sm", "ml-2");
       botaoRemover.textContent = "Remover";
       botaoRemover.addEventListener("click", () => {
-        estacionamento.removerVeiculo(veiculo.placa);
-        estacionamento.exibirVeiculos();
+        removerVeiculo(veiculo.placa);
       });
 
       itemLista.appendChild(botaoRemover);
       listaVeiculos.appendChild(itemLista);
-    }
+    });
   }
-}
+});
 
-// Função para exibir todos os veículos
-function exibirVeiculos() {
-  estacionamento.exibirVeiculos();
-}
+// limpar
+document.getElementById("limparBtn").addEventListener("click", () => {
+  limparCampos();
+});
 
 // Carregar os dados iniciais ao carregar a página
-document.addEventListener("DOMContentLoaded", carregarDadosIniciais);
-
+document.addEventListener("DOMContentLoaded", exibirVeiculos);
